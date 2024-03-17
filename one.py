3235a43a61ed7353c23bf7dd8b7eecb5af281e59
mv302 <103399066+mv302@users.noreply.github.com>
2024-03-17 23:43:13 +0530

    Update one.py

--- one.py
+++ one.py
@@ -1,29 +1,37 @@
 import streamlit as st
 import speech_recognition as sr
 from transformers import pipeline
+from audiorecorder import audiorecorder
 
 r = sr.Recognizer()
 
 def recognize(audio):
     st.write("Recording stopped")
-    try:
-        text = r.recognize_google(audio)
-        st.write("You said:", text)
-    except sr.UnknownValueError:
-        text = ""
-        st.write("Sorry, I could not understand audio.")
+    with sr.WavFile('audio.wav') as source:     
+        a = r.record(source)
+        try:
+            text = r.recognize_google(a)
+            st.write("You said:", text)
+        except sr.UnknownValueError:
+            text = ""
+            st.write("Sorry, I could not understand audio.")
+            exit()
 
     pipe = pipeline("text-classification")
-    st.write(pipe(text))
+    result = pipe(text)[0]["label"]
+    st.warning("Emotion detected")
+    st.subheader(f"You spoke in :blue[{result}] tone")
+    # st.write("Accuracy: ", result[0]["score"] )
     
 def main():
     st.title("Magnus")
-    with sr.Microphone() as source:  
-        record = st.button("Record", type="primary")
-        if record:
-            st.write('Recording...')
-            audio = r.listen(source)
-            recognize(audio)
+    audio = audiorecorder("Start recording", "Stop recording")
+    st.write(':green[Press press start recording and stop to analyse it\'s emotion]')
+    if len(audio) > 0:
+        st.audio(audio.export().read())  
+        audio.export("audio.wav", format="wav")
+        st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
+        recognize(sr.AudioFile("audio.wav"))
 
 if __name__ == "__main__":
     main()
